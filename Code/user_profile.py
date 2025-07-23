d8fee6b86ca83581c49af6d391703078b538ed61
Aria <ariadruker1@gmail.com>
2025-07-23 10:44:57 -0700

    updated user profile categories and excel summary

--- Code/user_profile.py
+++ Code/user_profile.py
@@ -15,10 +15,10 @@ def getUserProfile():
             while desired_growth != 1 and desired_growth != 2 and desired_growth != 3 and desired_growth != 4 and desired_growth != 5:
                 desired_growth = int(input('\n2. What are your growth goals? (a/b/c/d/5):\n\t1) Beat inflation (<3%)\n\t2) Modest and reliable (3-5%)\n\t3) Steady longterm (5-7%)\n\t4) Strong returns even with some bumps (7-9%)\n\t5) High growth with greater risk (10%+)\n'))
             while fluctuation != 1 and fluctuation != 2 and fluctuation != 3 and fluctuation != 4 and fluctuation != 5:
-                fluctuation = int(input('\n3. How much daily fluctuation is okay with you? (a/b/c/d/5):\n\t1) Not much at all (<3%)\n\t2) Small ups and downs are okay (3-6%)\n\t3) Regular market swings (6-10%)\n\t4) I can handle large moves if it helps grow (10-15%)\n\t5) Volatility doesn\' bother me (15%+)\n'))
+                fluctuation = int(input('\n3. How much daily fluctuation is okay with you? (a/b/c/d/5):\n\t1) Not much at all (<3%)\n\t2) Small ups and downs are okay (3-6%)\n\t3) Regular market swings (6-10%)\n\t4) I can handle large moves if it promotes growth (10-15%)\n\t5) Volatility doesn\' bother me (15%+)\n'))
             while worst_case != 1 and worst_case != 2 and worst_case != 3 and worst_case != 4 and worst_case != 5:
                 worst_case = int(input('\n4. In the worst case, what is the greatest loss you could tolerate? (a/b/c/d/5):\n\t1) Very low (<5%)\n\t2) Minor (<10%)\n\t3) Moderate (<20%)\n\t4) High (<35%)\n\t5) Very high (35%+)\n'))
         except ValueError:
             print("Invalid input. Please enter a number between 1 and 5.")
-
+ 
 getUserProfile()
