4452e84a42f66a7da8babd532772a05da4c71d8a
Aria <ariadruker1@gmail.com>
2025-08-01 23:23:18 -0700

    etf_data and max_drawdown integrated from user input

--- Code/main.py
+++ Code/main.py
@@ -4,15 +4,15 @@ Aria Druker and Linghe Zhou
 '''
 import user_profile as up
 import max_drawdown as md
+import pandas as pd
 from ishares_ETF_list import download_valid_data
 from Etf_Data import get_etf_data
+from datetime import datetime
 
 def main():
     user = up.getUserProfile()
     valid_tickers, data = download_valid_data()
-    md_tolerable_list = md.calculate_max_drawdown(user[3], valid_tickers, data)
-    for ticker in md_tolerable_list:
-        print(ticker, data[ticker]['Close'].dropna().tail())
+    end_date = pd.Timestamp(datetime.now())
+    md_tolerable_list = md.calculate_max_drawdown(user[3], user[4], valid_tickers, data, end_date)
     annual_data = get_etf_data(valid_tickers, user[0], md_tolerable_list, data)
-    print(annual_data)
 main()
